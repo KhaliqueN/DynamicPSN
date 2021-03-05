92a0df6bb66632aaae034daa8d022321ecb6cde4
KhaliqueN <78890192+KhaliqueN@users.noreply.github.com>
2021-03-05 15:52:18 -0600

    Add files via upload

--- scripts/app.r
+++ scripts/app.r
@@ -133,44 +133,33 @@ ui <- fluidPage(
 
     mainPanel(
 
-		width = 5,
+		width = 7,
 		tableOutput('upload'),
 
 		strong("Distance cutoff:"),
-		p("This parameter defines the largest 3-dimensional (3D) distance (in Angstroms) between any two amino acids
-		 in a protein domain to be considered as close enough. To make a PSN, amino acids are taken as nodes and any 
-		 two amino acids are are close enough are connected by an edge. The application currently has seven different 
-		 values to choose from, i.e., 4,5,6,...,10. In the original study, the value of 6 was used."),
+		p("To make a PSN, amino acids are taken as nodes and any two amino acids that are close enough are connected by an edge. The 'distance cutoff' parameter defines the largest 3-dimensional (3D) distance (in Angstroms) between any two amino acids in a protein domain to be considered as close enough. The application currently has seven different values to choose from, i.e., 4,5,6,...,10. In the original study, the value of 6 was used."),
 		br(),
 
 		strong("Number of amino acids:"),
 		p("A dynamic protein structure network (PSN) consists of two or more static PSN snapshots.
 			The 'number of amino acids' parameter defines the number of amino acids that are added
 		at each static protein structure network (PSN) snapshot, as the dynamic PSN is being created.
-			In more detail, given a value 'X' for the 'number of amino acids' parameter, a dynamic PSN is created as follows. 
-			Starting from the begining of a given protein sequence (i.e., the first amino acid towards the N-terminal 
-			of the protein), first X number of amino acids are converted into the first static PSN snapshot, then first 2X 
-			number of amino acids are converted into the second static PSN snapshot, then first 3X number of amino acids 
-			are converted into the thrid static PSN snapshot, and so on until all of the amino acids are captured in the 
-			last snapshot of the corresponding dynamic PSN. In the original study, a value of 5 was used. In the application
-			 16 different the values (5,6,7,...,20) can be used."),
+			In more detail, given a value 'X' for the 'number of amino acids' parameter, a dynamic PSN is created as follows. Starting from the begining of a given protein sequence (i.e., the first amino acid towards the N-terminal 
+			of the protein), first X number of amino acids are converted into the first static PSN snapshot, then first 2X number of amino acids are converted into the second static PSN snapshot, then first 3X number of amino acids are converted into the thrid static PSN snapshot, and so on until all of the amino acids in the sequence are captured in the last snapshot of the corresponding dynamic PSN. In the original study, a value of 5 was used. In the application 16 different the values (5,6,7,...,20) can be used."),
 		br(),
 
 		strong("Select task:"),
 		p("This parameter controls the extent of the application that the user wants to run.
 			There are three options: Mode 1, Mode 2, and Mode 3.
-			If a user wants to just create the dynamic networks, then the option 'Mode 1' should be chosen.
-			If a user wants to create the dynamic networks and also extract dynamic graphlet-based features of the dynamic networks,
-			 then the option 'Mode 2' should be chosen.
-			If a user wants to create the dynamic networks, extract dynamic graphlet-based features of the dynamic networks, and 
-			classify the protein doamins using the resulting dynamic features,
+			If a user wants to just create dynamic networks, then the option 'Mode 1' should be chosen.
+			If a user wants to create dynamic networks and also extract dynamic graphlet-based features of the dynamic networks, then the option 'Mode 2' should be chosen.
+			If a user wants to create dynamic networks, extract dynamic graphlet-based features of the dynamic networks, and classify the protein domains using the resulting dynamic features,
 			 then the option 'Mode 3' should be chosen.
 			"),
 		br(),
 
 		strong("Select CIF folder:"),
-		p("This is a path to the directory where all of the Cyrstallographic In formation File 
-			(CIF) of the protein 3-dimansional structures are stored in your local computing machine.
+		p("This is a path to the directory where all of the Cyrstallographic Information File (CIF) of the protein 3-dimensional structures are stored in your local computing machine.
 			"),
 		br(),
 
@@ -179,17 +168,15 @@ ui <- fluidPage(
 			The contents of the file should be in a two column format, with the two columns being separated by a 'tab'.
 			The first column should be the structual class of the corresponding protein domain in the second column. 
 			This structural class information is only used in the protein structural classification part of the software. 
-			If you do not want the structural part of the proteins in your set and if you do not wish to perform 
+			If you do not have structural class information of the proteins in your dataset and if you do not wish to perform 
 			protein structural classification, then please add a dummy structural class for each of the proteins in your list.
-			In the second column, the name of the protein domain should be given as 'PDBID_chainID_startID_endID'. 
-			Here, PDBID is the name of the PDB file for example, for the PDB file '155c.pdb' the PDBID is '155c'.
-			chainID is the idenfication of the protein chain to which the corresponding protein domain belongs.
-			startID is the sequence number of the chainID where the corresponding protein domain starts.
-			endID is the sequence number of the chainID where the corresponding protein domain ends. 
-			For example, sequence number 1 to 134 in the chain A of the PDB file '155c.pdb' belongs to the protein structural class of 
-			'alpha'. In this case, the corresponsing line in the annotation file would be 'alpha	[tab separation] 155c_A_1_134'.
-			If the protein domain you want to process spans the whole protein chain, then please write the first sequence number 
-			of the chain as startID and the last sequence number of the chain as the endID."
+			In the second column, the name of the protein domain should be given as 'CIFID_chainID_startID1_endID1-startID2_endID2-...-startIDn_endIDn'. 
+			Here, CIFID is the name of the CIF file. For example, for the CIF file '1fnn.cif' the CIFID is '1fnn'.
+			chainID is the idenfication of the protein chain to which the corresponding protein domain belongs. startIDs and ednIDs represent the starting and ending sequence number for each of the contiguous amino acid sequence segments of the corresponding protein domain. For example, if a protein domain has two segments that forms that domain, then startID1 will represent the sequence number of the chainID where the corresponding first segment starts, and endID1 will represent the sequence number of the chainID where the corresponding first segment ends. Additionally, startID2 will represent the sequence number of the chainID where the corresponding second segment starts, and endID2 will represent the sequence number of the chainID where the corresponding second segment ends.
+			For example, sequence number 1 to 17 in the chain A of the CIF file '1fnn.cif' belongs to the segment 1 and 
+			sequence number 192 to 275 in the chain A of the CIF file '1fnn.cif' belongs to the segment 2 of the same protein domain protein that belongs to the structural class of 'alpha'. 
+			In this case, the corresponsing line in the annotation file should be 'alpha	[tab separation] 1fnn_A_1_17-192_275'. As another example, sequence number 2 to 54 in the chain A of the CIF file '1aip.pdb' belongs to the segment 1, which is the only segment of contiugous amino acids of the protein domain, belongs to the structural class of 'alpha'. In this case, the corresponsing line in the annotation file should be 'alpha	[tab separation] 1aip_C_2_54'.
+			If the protein domain you want to process spans the whole protein chain, then please write the first sequence number of the chain as startID and the last sequence number of the chain as the endID."
 			
 			),
 
